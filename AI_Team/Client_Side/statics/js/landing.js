3e3158fabf6b5b33bcafbda96fdc9b3d6c346c09
Santiago Reina <rsanty.jw@gmail.com>
2023-09-07 00:28:07 -0500

    aiteam just a gpt-4, css upgrade but little wrongs

--- AI_Team/Client_Side/statics/js/landing.js
+++ AI_Team/Client_Side/statics/js/landing.js
@@ -1,3 +1,4 @@
+// Function to toggle the animation of dots.
 function toggleDotsAnimation(shouldShow) {
     const loadingDots = document.querySelector('.loading-dots-container');
     const metallicText = document.querySelector('.metallic-text');
@@ -7,20 +8,26 @@ function toggleDotsAnimation(shouldShow) {
     metallicText.style.display = displayValue;
 }
 
-function sendMessage() {
-    let message = document.getElementById("userMessage").value;
+// Helper function to retrieve a cookie value by name.
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
 
-    if(message.trim() !== "") {
-        toggleDotsAnimation(true);
+// Function to send the user's message and receive the response.
+function addUserMessage() {
+    const message = document.getElementById("userMessage").value;
 
-        let csrfToken = getCookie('csrftoken'); 
-
+    if (message.trim() !== "") {
+        const csrfToken = getCookie('csrftoken');
+        toggleDotsAnimation(true)
         fetch("/aiteam/", {
             method: "POST",
-            body: new URLSearchParams({ "message": message }),
+            body: new URLSearchParams({ "message": message, "phase": "user_message" }),
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded",
-                "X-CSRFToken": csrfToken 
+                "X-CSRFToken": csrfToken
             }
         })
         .then(response => {
@@ -30,51 +37,96 @@ function sendMessage() {
             return response.json();
         })
         .then(data => {
-            console.log(`Usuario: ${message}`);
-            console.log(`IA: ${data.ai_response}`);
-            toggleDotsAnimation(false);
+            const chatBox = document.getElementById("chatBox");
+            chatBox.insertAdjacentHTML('beforeend', data.user_message_div);
+            
+            // Now make a request for the AI's response here, inside the .then()
+            return fetch("/aiteam/", {
+                method: "POST",
+                body: new URLSearchParams({ "message": message, "phase": "ai_response" }),
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "X-CSRFToken": csrfToken
+                }
+            });
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const chatBox = document.getElementById("chatBox");
+            chatBox.insertAdjacentHTML('beforeend', data.ia_message_div);
         })
         .catch(error => {
             console.error('Error:', error);
-            toggleDotsAnimation(false);
         });
-
+        toggleDotsAnimation(false)
+        // Clear the user input field.
         document.getElementById("userMessage").value = "";
     }
 }
 
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
+// Function to receive the AI's response.
+function sendMessage(message) {
+    //const message = document.getElementById("userMessage").value;
+    const csrfToken = getCookie('csrftoken');
+
+    fetch("/aiteam/", {
+        method: "POST",
+        body: new URLSearchParams({ "message": message, "phase": "ai_response" }),
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "X-CSRFToken": csrfToken
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        if (data.error) {
+            console.error('Error from server:', data.error);
+        } else {
+            const chatBox = document.getElementById("chatBox");
+            chatBox.insertAdjacentHTML('beforeend', data.ia_message_div);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+    // Clear the user input field.
+    document.getElementById("userMessage").value = "";
 }
 
-// Send message
+// Handle Enter key press to send the message.
 function handleKeyDown(event) {
-    if (event.keyCode === 13) {  // 13 es el keyCode para la tecla Enter
-        sendMessage();
-        event.preventDefault();  // Evita que se realice la acción por defecto del Enter
+    if (event.keyCode === 13) {  // 13 is the keyCode for Enter key.
+        addUserMessage();
+        event.preventDefault();  // Prevents the Enter action from triggering a page reload.
     }
 }
 
-//Event listeners for send messages
-const sendButton = document.querySelector('.btn-send');
-sendButton.addEventListener('click', sendMessage);
-
-const userMessageInput = document.getElementById('userMessage');
-sendButton.addEventListener('keydown', handleKeyDown);
-
-
+// Initialize event listeners.
 document.addEventListener("DOMContentLoaded", function() {
-    // Hamburger toggle
+    // Send message using the button.
+    const sendButton = document.querySelector('.btn-send');
+    sendButton.addEventListener('click', addUserMessage);
+
+    // Send message using the Enter key.
+    const userMessageInput = document.getElementById('userMessage');
+    userMessageInput.addEventListener('keydown', handleKeyDown);
+
+    // Toggle hamburger menu.
     const hamburgerToggle = document.querySelector("#hamburgerToggle");
     const menuItems = document.querySelector(".menu-items");
 
     hamburgerToggle.addEventListener("change", function() {
-        if (this.checked) {
-            menuItems.style.transform = 'translateX(0)';
-        } else {
-            menuItems.style.transform = 'translateX(-100%)';
-        }
+        menuItems.style.transform = this.checked ? 'translateX(0)' : 'translateX(-100%)';
     });
 });
--- AI_Team/Server_Config/Server_Side/views.py
+++ AI_Team/Server_Config/Server_Side/views.py
@@ -1,35 +1,49 @@
+from django.http import JsonResponse
 from django.shortcuts import render
-from django.http import JsonResponse, HttpResponse
-import google.generativeai as palm
+from django.template.loader import render_to_string
 from AI_Team.Logic.LLMs import Call
-#key = 'AIzaSyDW2ul61CVxQCyFM591byBSyC587YDey7o'
 
-def chat_ui(request):  
+# We'll start by creating a helper function to render the HTML for messages.
+def render_html(template, message):
+    """Helper function to render the message to HTML."""
+    return render_to_string(template, {"message": message})
+
+def chat_ui(request):
+    # Initializing the response_data dictionary.
+    response_data = {}
+
+    # Ensure the request is a POST request.
     if request.method == "POST":
-        """ obtener e imprimir key del ambiente
-        api_key = os.getenv("Palm2APIKey")
-
-        # Check if the key exists and print it
-        if api_key:
-            print(f"The API key is: {api_key}")"""
-
         user_message = request.POST.get('message')
-        print('user_message', user_message)
-        response_data = {}
-        response = None
-        try:
-            response = Call(user_message,"Palm2")
-        except Exception as e:
-            response_data['error'] = f"Something went wrong: {e}"      
+        print('get the message',user_message)
+        phase = request.POST.get('phase')
 
-        if response:
-            response_data['ai_response'] = response
-        else:
-            response_data['ai_response'] = 'the api dont could response'
+        # Handle the user's message.
+        if phase == 'user_message':
+            response_data['user_message_div'] = render_html('chat_messages/user_message.html', user_message)
+            return JsonResponse(response_data)
+
+        # Handle the AI's response.
+        elif phase == 'ai_response':
+            try:
+                ai_response = Call(user_message, "Palm2")
+            except Exception as e:
+                response_data['error'] = f"Something went wrong: {e}"
+                return JsonResponse(response_data, status=500)  # Respond with a 500 status code for internal errors.
+
+            # Ensure the AI returned a response.
+            if ai_response:
+                response_data['ia_message_div'] = render_html('chat_messages/ia_message.html', ai_response)
+            else:
+                response_data['error'] = 'The API could not respond.'
+                return JsonResponse(response_data, status=400)  # Respond with a 400 status code for bad requests.
             
-        return JsonResponse(response_data)
+            return JsonResponse(response_data)
 
+    # Render the chat UI template for non-POST requests.
     return render(request, 'chat_ui.html')
+
+
 
 def login(request):  
     if request.method == 'POST':
