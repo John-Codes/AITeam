4fc3318c00c01acc16404baa16189f6ee82dbfad
Santiago Reina <rsanty.jw@gmail.com>
2023-09-11 22:45:35 -0500

    urls login, logout and resetpassword

--- AI_Team/Logic/sender_mails.py
+++ AI_Team/Logic/sender_mails.py
@@ -2,13 +2,20 @@ from django.core.mail import send_mail
 from django.conf import settings
 
 def email_send(prompt):
-    try:
-        subject = 'Se ha enviado una pregunta por email'
-        message = f"""Un usuario ha enviado la seguiente pregunta: {prompt}"""
+    # check variables
+        subject = 'La función de envio de email ya esta enviando correos'
+        message = f"""
+        Hola soy tu asistente IA de AITEAM y he detectado una pregunta de un usuario con correo: correo_ de usuario
+        ahora sigue analizar la respuesta en busca de correos para que se envie y convertir eso en una variable global 
+        para que envie preguntas del usuario de toda la conversación, el correo que llegue seguirá diciendo:
+        Solo tu puedes responder la pregunta: (a continuación el prompt)
+        {prompt}"""
         from_email = settings.EMAIL_HOST_USER
-        recipient_list = ['destinatario@example.com']
-    
-        send_mail(subject, message, from_email, recipient_list)
-        print('correo enviado')
-    except Exception as e:
-        print('correo no enviado porque:', e)
+        print('this is from email:',from_email)
+        recipient_list = ['rsanty.jw@gmail.com', 'efexzium@gmail.com']
+        print('all config variables will get')        
+        try:
+            send_mail(subject, message, from_email, recipient_list)
+            print('correo enviado')
+        except Exception as e:
+            print('correo no enviado porque:', e)
---
+++ AI_Team/Server_Config/Server_Side/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True, help_text="Required.")
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+class PasswordResetForm(forms.Form):
+    username = forms.CharField(label='Username', max_length=150)
+    email = forms.EmailField(label='Email')
+    newpassword = forms.CharField(label='New Password', widget=forms.PasswordInput)
+    confirmpassword = forms.CharField(label='Confirm New Password', widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        email = cleaned_data.get('email')
+        newpassword = cleaned_data.get('newpassword')
+        confirmpassword = cleaned_data.get('confirmpassword')
+
+        try:
+            user = User.objects.get(username=username, email=email)
+        except User.DoesNotExist:
+            print("DEBUG: User does not exist with given username and email.")
+            raise forms.ValidationError("Username and Email do not match an existing user.")
+
+        if newpassword != confirmpassword:
+            print("DEBUG: Passwords entered do not match.")
+            raise forms.ValidationError("Passwords do not match.")
+        
+        return cleaned_data
