015c4132f4785ffd157f85520ce7ec45f01aa8a1
Santiago Reina <rsanty.jw@gmail.com>
2023-10-19 12:14:52 -0500

    add a new folder for deployment

--- AI_Team/Server_Config/settings.py
+++ AI_Team/Server_Config/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "Client_Side", "statics")] # path django searches the static files (images, styles in css and java scripts) for loaded 
-STATIC_ROOT = os.path.join(BASE_DIR, "Client_Side", "statics")
+STATIC_ROOT = os.path.join(BASE_DIR, "Client_Side", "statics_deployment")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
