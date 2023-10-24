3fd3c30d5c64b65ba9b6c79e441982ac3605b638
Santiago Reina <rsanty.jw@gmail.com>
2023-10-24 08:13:41 -0500

    database update

--- AI_Team/Server_Config/Server_Side/models.py
+++ AI_Team/Server_Config/Server_Side/models.py
@@ -32,15 +32,24 @@ class PaymentMethod(models.Model):
 class Client(AbstractUser):
     class Meta:
         app_label = 'Server_Side'
-        verbose_name_plural = "Clientes" 
+        verbose_name_plural = "Clientes"
+
+    def get_default_current_plan():
+        return Current_Plan.objects.get(id=1).id
+
+    def get_default_subscription_status():
+        return SubscriptionStatus.objects.get(id=1).id
+
+    def get_default_payment_method():
+        return PaymentMethod.objects.get(id=1).id
 
     is_subscribe = models.BooleanField(default=False)
     order_id = models.CharField(max_length=255, null=True, blank=True)
     last_transaction_status = models.CharField(max_length=50, null=True, blank=True)
-    current_plan = models.ForeignKey(Current_Plan, on_delete=models.CASCADE, default=1)
-    subscription_status = models.ForeignKey(SubscriptionStatus, on_delete=models.CASCADE, default=1)
+    current_plan = models.ForeignKey(Current_Plan, on_delete=models.CASCADE, default=get_default_current_plan)
+    subscription_status = models.ForeignKey(SubscriptionStatus, on_delete=models.CASCADE, default=get_default_subscription_status)
     id_pay = models.PositiveIntegerField(null=True, blank=True)
-    method_pay = models.ForeignKey(PaymentMethod, on_delete=models.CASCADE, default=1)
+    method_pay = models.ForeignKey(PaymentMethod, on_delete=models.CASCADE, default=get_default_payment_method)
     next_date_pay = models.DateField(null=True, blank=True)
     date_subscription = models.DateField(null=True, blank=True)
 
