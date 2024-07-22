36506fe4b17a705d903b9de9a1e9ac36087146c2
johnny rodriguez <efexzium@gmail.com>
2024-07-22 11:04:05 -0400

    Fixed Statics deployment bug

--- AI_Team/Server_Config/urls.py
+++ AI_Team/Server_Config/urls.py
@@ -30,3 +30,5 @@ urlpatterns = i18n_patterns(
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else :
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
